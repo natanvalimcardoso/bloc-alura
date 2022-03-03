c7946f108d0db0e63127ce6a27451c626b4102ec
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-03-03 15:52:26 -0300

    ready

--- android/app/src/main/kotlin/com/example/bloc_alura/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bloc_alura
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/bytebank_v4/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bytebank_v4
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
